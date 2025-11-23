1cdaf9046bc4810f7dd4eb3382552daac79257a3
D-Berg <danielfjberg@gmail.com>
2025-11-23 13:09:50 +0100

    window logic is better

--- src/App.zig
+++ src/App.zig
@@ -13,8 +13,9 @@ const MAX_SEARCH_LEN = 1024;
 
 tty: *Tty,
 arena_state: std.heap.ArenaAllocator.State,
-/// indexes into choices
 matches: []Match,
+/// window into matches
+window: []const Match,
 selected: usize = 0,
 search_str: []u8,
 search_buf: [MAX_SEARCH_LEN]u8,
@@ -37,6 +38,7 @@ pub fn init(gpa: Allocator, tty: *Tty, choices: []const []const u8) !App {
     app.matches = matches;
     app.search_buf = undefined;
     app.arena_state = arena_impl.state;
+    app.window = matches[0..];
 
     return app;
 }
@@ -49,11 +51,12 @@ pub fn deinit(app: *App, gpa: Allocator) void {
 pub fn run(app: *App, io: Io, gpa: Allocator, result: *?[]const u8) !void {
     const tty = app.tty;
     app.search_str = app.search_buf[0..0];
+
     _ = io;
 
     try app.draw();
 
-    try app.updateMatches(gpa);
+    app.window = try updateMatches(gpa, app.search_str, app.matches);
 
     var buf: [1]u8 = undefined;
     while (true) {
@@ -64,10 +67,21 @@ pub fn run(app: *App, io: Io, gpa: Allocator, result: *?[]const u8) !void {
         if (maybe_c) |c| switch (c) {
             util.ctrl('c') => return,
             util.ctrl('p') => {
-                if (app.selected < app.matches.len) app.selected += 1;
+                if (app.selected + 1 < app.window.len) {
+                    app.selected += 1;
+                } else {
+                    app.selected = 0;
+                }
             },
             util.ctrl('n') => {
-                if (app.selected > 0) app.selected -= 1;
+                if (app.selected > 0) {
+                    app.selected -= 1;
+                } else {
+                    app.selected = if (app.window.len > 0)
+                        app.window.len - 1
+                    else
+                        0;
+                }
             },
             127 => {
                 // backspace
@@ -75,11 +89,13 @@ pub fn run(app: *App, io: Io, gpa: Allocator, result: *?[]const u8) !void {
                     app.search_str.len -= 1;
                 }
 
-                try app.updateMatches(gpa);
+                app.window = try updateMatches(gpa, app.search_str, app.matches);
             },
 
             '\r', '\n' => {
-                result.* = app.matches[app.selected].original_str;
+                if (app.selected < app.window.len) {
+                    result.* = app.window[app.selected].original_str;
+                }
                 return;
             },
             else => {
@@ -88,7 +104,7 @@ pub fn run(app: *App, io: Io, gpa: Allocator, result: *?[]const u8) !void {
                     app.search_str.len += 1;
                     app.search_str[at] = c;
 
-                    try app.updateMatches(gpa);
+                    app.window = try updateMatches(gpa, app.search_str, app.matches);
                 }
             },
         };
@@ -110,15 +126,17 @@ pub fn draw(app: *App) !void {
     // TODO: handle input longer than tty width
     try tty.print("{s}{s}", .{ prompt, app.search_str });
 
-    const max_window_height = @min(tty.max_height - 1, app.matches.len);
-    var start: usize = 0;
+    const max_window_height = @min(tty.max_height - 1, app.window.len);
 
+    var start: usize = 0;
     if (app.selected >= max_window_height) {
-        start = app.selected - max_window_height + 1;
+        start = app.selected - max_window_height;
     }
 
     var end = start + max_window_height;
-    if (end > app.matches.len) end = app.matches.len;
+    if (end >= app.window.len) end = app.window.len;
+
+    assert(end >= start);
 
     var row = tty.max_height - 1;
     for (start..end) |i| {
@@ -126,31 +144,34 @@ pub fn draw(app: *App) !void {
 
         try tty.clearLine();
 
-        const match = app.matches[i];
+        const match = app.window[i];
 
         const str = match.original_str;
-        const max_len = @min(tty.max_width - 10, str.len);
+        const max_width = @min(tty.max_width - 10, str.len);
 
-        if (app.search_str.len == 0 or match.score > Match.score_min) {
-            if (app.selected == i) {
-                try tty.print("\x1b[38;2;197;41;96m", .{}); // red
-                try tty.print("\x1b[48;2;100;100;100m", .{});
-                try tty.print("▌", .{});
-                try tty.print("\x1b[m", .{});
+        if (app.selected == i) {
+            try tty.print("\x1b[38;2;197;41;96m", .{}); // red
+            try tty.print("\x1b[48;2;100;100;100m", .{});
+            try tty.print("▌", .{});
+            try tty.print("\x1b[m", .{});
 
-                try tty.print("\x1b[48;2;100;100;100m", .{});
+            try tty.print("\x1b[48;2;100;100;100m", .{});
 
-                if (builtin.mode == .Debug) try tty.print("({d:2})", .{match.score});
-                try tty.print("  {s}", .{str[0..max_len]});
-                try tty.print("\x1b[m", .{});
-            } else {
-                try tty.print("\x1b[38;2;100;100;100m▌\x1b[m", .{});
-                if (builtin.mode == .Debug) try tty.print("({d:2})", .{match.score});
-                try tty.print("  {s}", .{str[0..max_len]});
-            }
+            if (builtin.mode == .Debug) try tty.print("({d:.2})", .{match.score});
+
+            try tty.print("  {s}", .{str[0..max_width]});
+            try tty.print("\x1b[m", .{});
+        } else {
+            try tty.print("\x1b[38;2;100;100;100m▌\x1b[m", .{});
+            if (builtin.mode == .Debug) try tty.print("({d:.2})", .{match.score});
+            try tty.print("  {s}", .{str[0..max_width]});
         }
 
         row -= 1;
+    }
+    while (row >= 1) : (row -= 1) {
+        try tty.moveTo(row, 1);
+        try tty.clearLine();
     }
 
     try tty.moveTo(tty.max_height, prompt.len + app.search_str.len + 1);
@@ -162,21 +183,28 @@ pub fn draw(app: *App) !void {
 }
 
 // TODO: update concurrently
-pub fn updateMatches(app: *App, gpa: Allocator) !void {
-    if (app.search_str.len == 0) {
+/// Returns a slice into matches and update each match score
+pub fn updateMatches(gpa: Allocator, search_str: []const u8, matches: []Match) ![]const Match {
+    if (search_str.len == 0) {
         // restore to original
-        Match.sortMatches(app.matches, Match.orderByIdx);
-        return;
+        Match.sortMatches(matches, Match.orderByIdx);
+        for (matches) |*match| match.score = Match.score_min;
+        return matches[0..];
     }
 
     var buf: [MAX_SEARCH_LEN]u8 = undefined;
-    const needle = util.lowerString(&buf, app.search_str);
+    const needle = util.lowerString(&buf, search_str);
 
-    for (app.matches) |*match| {
+    for (matches) |*match| {
         try match.updateScore(gpa, needle);
     }
 
-    // TODO: create a window of matches with score > 0
+    Match.sortMatches(matches, Match.orderByScore);
 
-    Match.sortMatches(app.matches, Match.orderByScore);
+    var len: usize = 0;
+    for (matches) |match| {
+        if (match.score <= 0) break;
+        len += 1;
+    }
+    return matches[0..len];
 }
